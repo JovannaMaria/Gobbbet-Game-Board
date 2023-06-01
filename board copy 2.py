f744d0396ec494cb5658ae8e392659f5cd9b01e1
JovannaMaria <115571588+JovannaMaria@users.noreply.github.com>
2023-06-01 15:28:26 +0530

    need to fix bugs

--- board copy 2.py
+++ board copy 2.py
@@ -3,12 +3,15 @@ from collections import deque
 # Define the empty and blocked spots on the board
 EMPTY_SPOT = '-'
 BLOCKED_SPOT = 'X'
-
+# create dictionary
+d={'A':3,'B':2,'C':1,'X':3,'Y':2,'Z':1}
+x=list('ABC')
+y=list('XYZ')
 # Create the stacks
-s1 = deque([1,2,3])
-s2 = deque([1,2,3])
-s3 = deque([1,2,3])
-s4 = deque([1,2,3])
+s1 = deque(['C','B','A'])
+s2 = deque(['C','B','A'])
+s3 = deque(['Z','Y','X'])
+s4 = deque(['Z','Y','X'])
 px=[s1,s2]
 py=[s3,s4]
 class GobbletBoard:
@@ -38,28 +41,31 @@ class GobbletBoard:
         if from_row == to_row and from_col == to_col:
             return False
         
-        # Check if the source spot is not empty
-        if self.board[from_row][from_col][-1] == EMPTY_SPOT:
-            return False
         
         # Check if new piece is moved
         if (curr=='Player 1' and 4==from_row and 0<= from_col <2):
             x=px[from_col][0]
             if self.board[to_row][to_col][-1] == EMPTY_SPOT:
-                self.board[to_row][to_col].pop()
-                self.board[to_row][to_col].append(self.board[from_row][from_col].pop())
-            elif x > self.board[to_row][to_col][-1]:
+                print(self.board[to_row][to_col])
+                self.board[to_row][to_col].append(px[from_col].pop())
+            elif d[x] > d[self.board[to_row][to_col][-1]]:
                 self.board[to_row][to_col].append(px[from_col].pop()) 
-                return True
+            return True
         elif (curr=='Player 2' and 5==from_row and 0<= from_col <2):
             y=py[from_col][0]
             if self.board[to_row][to_col][-1] == EMPTY_SPOT:
                 self.board[to_row][to_col].pop()
-                self.board[to_row][to_col].append(self.board[from_row][from_col].pop())
-            if y > self.board[to_row][to_col]:
                 self.board[to_row][to_col].append(py[from_col].pop())
-                return True 
+            if d[y] > d[self.board[to_row][to_col][-1]]:
+                self.board[to_row][to_col].append(py[from_col].pop())
+            return True 
         
+        if not (0 <= from_row < 3 and 0 <= from_col < 3 and
+                0 <= to_row < 3 and 0 <= to_col < 3) :
+            return False
+        # Check if the source spot is not empty
+        if self.board[from_row][from_col][-1] == EMPTY_SPOT:
+            return False
         # Check if the destination spot is empty or has a smaller piece on top
         if self.board[to_row][to_col][-1] == EMPTY_SPOT:
             self.board[to_row][to_col].pop()
@@ -68,7 +74,7 @@ class GobbletBoard:
                 self.board[from_row][from_col].append(EMPTY_SPOT)
             return True
         
-        elif self.board[from_row][from_col][-1] > self.board[to_row][to_col][-1]:
+        elif d[self.board[from_row][from_col][-1]] > d[self.board[to_row][to_col][-1]]:
             self.board[to_row][to_col].append(self.board[from_row][from_col].pop())
             if len(self.board[from_row][from_col])==0:
                 self.board[from_row][from_col].append(EMPTY_SPOT)
@@ -120,9 +126,11 @@ def play_game():
         if not(board.is_valid_move(from_row, from_col, to_row, to_col,current_player)):
             print('Invalid move. Try again.')
         else:
+            board.print_board()
             # Check if the current player has won
-            if check_win_condition(board, current_player):
-                print(f"{current_player} wins!")
+            t,winner= check_win_condition(board)
+            if t:
+                print(f"{winner} wins!")
                 break
             
             # Switch to the other player
@@ -145,24 +153,32 @@ def play_game():
             print('Invalid move. Try again.')'''
 
 # Function to check win condition
-def check_win_condition(board, player):
+def check_win_condition(board):
     # Check rows
     for row in board.board:
-        if row[0] == row[1] == row[2] == player:
-            return True
+        if row[0][-1] in x and row[1][-1] in x and row[2][-1] in x: 
+            return True,'Player 1'
+        if row[0][-1] in y and row[1][-1] in y and row[2][-1] in y: 
+            return True,'Player 2'
     
     # Check columns
     for col in range(3):
-        if board.board[0][col] == board.board[1][col] == board.board[2][col] == player:
-            return True
+        if board.board[0][col][-1] in x and board.board[1][col][-1] in x and board.board[2][col][-1] in x:
+            return True,'Player 1'
+        if board.board[0][col][-1] in y and board.board[1][col][-1] in y and board.board[2][col][-1] in y:
+            return True,'Player 2'
     
     # Check diagonals
-    if board.board[0][0] == board.board[1][1] == board.board[2][2] == player:
-        return True
-    if board.board[0][2] == board.board[1][1] == board.board[2][0] == player:
-        return True
+    if board.board[0][0][-1] in x and board.board[1][1][-1] in x and board.board[2][2][-1] in x:
+        return True,'Player 1'
+    if board.board[0][2][-1] in x and board.board[1][1][-1] in x and board.board[2][0][-1] in x:
+        return True,'Player 1'
+    if board.board[0][0][-1] in y and board.board[1][1][-1] in y and board.board[2][2][-1] in y:
+        return True,'Player 1'
+    if board.board[0][2][-1] in y and board.board[1][1][-1] in y and board.board[2][0][-1] in y:
+        return True,'Player 1'
     
-    return False
+    return False,''
 
 # Start the game
 play_game()
